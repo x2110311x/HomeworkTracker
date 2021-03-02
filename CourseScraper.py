a845b6f3b105cc82853855052c7c9a3e8db96a04
woeffner <58149950+woeffner@users.noreply.github.com>
2021-03-02 17:26:27 -0500

    Refactored Scraper

--- CourseScraper.py
+++ CourseScraper.py
@@ -2,58 +2,82 @@ from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
 import csv
 
-url = 'http://catalog.kent.edu/coursesaz/'
-
-uClient = uReq(url)
-page_html = uClient.read()
-uClient.close()
-
-page_soup = soup(page_html, "html.parser")
-
-class_list_html = page_soup.findAll("div", {"id": "atozindex"})[0].findAll("ul", {})
-
 class_list = []
-
 class_links = []
-
-for class_container in class_list_html:
-    for class_name in class_container.findAll('li', {}):
-        class_list.append(class_name.text)
-        if(class_name.a['href'] != "/coursesaz/seeall/"):
-            class_links.append("http://catalog.kent.edu" + class_name.a['href'])
-
-
-with open("categories.csv", 'w') as f:
-    write = csv.writer(f, delimiter = '\n')
-
-    write.writerow(class_list)
-
-
-with open("class_links.csv", 'w') as f:
-    write = csv.writer(f, delimiter = '\n')
-
-    write.writerow(class_links)
-
 course_titles = []
 
-for class_link in class_links:
-    print(class_link)
+
+def parse_class_list():
+    page_soup = get_html_soup()
+
+    get_classes(page_soup)
+
+    write_all_lists_to_file()
+
+
+def get_html_soup():
+    url = 'http://catalog.kent.edu/coursesaz/'
+    
+    page_html = get_html(url)
+
+    page_soup = soup(page_html, "html.parser")
+
+    return page_soup
+
+
+def get_html(url):
+    uClient = uReq(url)
+    page_html = uClient.read()
+    uClient.close()
+    return page_html
+
+
+def get_classes(page_soup):
+    class_list_html = page_soup.findAll("div", {"id": "atozindex"})[0].findAll("ul", {})
+
+    retrieve_class_list(class_list_html)
+
+    for class_link in class_links:
+        course_soup = get_course_soup(class_link)
+        
+
+        for title in course_soup:
+            retrieve_course_names(title)
+
+
+def retrieve_class_list(list_html):
+    for class_container in list_html:
+        for class_name in class_container.findAll('li', {}):
+            class_list.append(class_name.text)
+            if(class_name.a['href'] != "/coursesaz/seeall/"):
+                class_links.append("http://catalog.kent.edu" + class_name.a['href'])
+
+
+def write_list_to_file(info_list, file):
+    with open(file, 'w') as f:
+        write = csv.writer(f, delimiter = '\n')
+        write.writerow(info_list)
+
+
+def get_course_soup(class_link):
     uClient = uReq(class_link)
     page_html = uClient.read()
     uClient.close()
 
     page_soup = soup(page_html, "html.parser")
 
-    course_list_html = page_soup.findAll("div", {"class": "sc_sccoursedescs"})[0].findAll("p", {"class": "courseblocktitle"})
-
-    for title in course_list_html:
-        segments = title.text.split('\t')
-        course_titles.append(','.join(segments[0:1]))
+    return page_soup.findAll("div", {"class": "sc_sccoursedescs"})[0].findAll("p", {"class": "courseblocktitle"})
 
 
+def retrieve_course_names(title):
+    segments = title.text.split('\t')
+    course_titles.append(','.join(segments[0:1]))
 
 
-with open("course_titles.csv", 'w') as f:
-    write = csv.writer(f, delimiter = '\n')
+def write_all_lists_to_file():
+    write_list_to_file(class_list, "categories.csv")
+    write_list_to_file(class_links, "class_links.csv")
+    write_list_to_file(course_titles, "course_titles.csv")
+    
 
-    write.writerow(course_titles)
+parse_class_list()
