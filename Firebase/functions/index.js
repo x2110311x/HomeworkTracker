9afdca42d661d4c678fb24f9661da5231cb49383
Alex Sweeney <x2110311x@gmail.com>
2021-03-02 14:21:51 -0500

    Add form post to backend

--- Firebase/functions/index.js
+++ Firebase/functions/index.js
@@ -164,39 +164,44 @@ app.get('/reset', checkCookieMiddleware, (request, response) => {
 });
 
 
-
+app.post("/addTask", checkCookieMiddleware, (request, response) => {
+    if (request.signedin) {
+        var name = request.body.name;
+        var estTime = request.body.estTime;
+        var dueDate = request.body.dueDate;
+        var description = request.body.description;
+        var priority = request.body.priority;
+        var scheduledTimeStart = request.body.scheduledTimeStart;
+        var scheduledTimeEnd = request.body.scheduledTimeEnd;
+        
+        const data = {
+            name: name,
+            est_time: estTime,
+            due_date: dueDate,
+            description: description,
+            completed: false,
+            priority: priority,
+            scheduled_time_start: scheduledTimeStart,
+            scheduled_time_end: scheduledTimeEnd,
+        };
+        
+        // Add a new task
+        const writeResult = admin.firestore().collection('Users')
+        .doc(request.decodedClaims.uid).collection('tasks').set(data)
+        .then(() => {
+            response.send(200).send("Task successfully added");
+            console.log("Task has been added,", writeResult.id);
+        }).catch((error) => {
+            console.error("Error writing document: ", error);
+            response.send(500).send("Error writing document: ", error.message);
+        });
+    } else {
+        response.status(403).send("Unauthorized");
+    }
+});
 
 app.get('*', (request, response) => {
     response.status(404).render('404');
-});
-
-app.post("/addTask", checkCookieMiddleware, (request, response) => { //NOTE: CHANGED THIS TO ADD TASK SINCE WE WILL HAVE MULTIPLE TASK PAGES
-  if (request.method == "POST" && request.signedin) {
-    var name = request.body.name;
-    var estTime = request.body.estTime;
-    var dueDate = request.body.dueDate;
-    var description = request.body.description;
-    var priority = request.body.priority;
-    var scheduledTimeStart = request.body.scheduledTimeStart;
-    var scheduledTimeEnd = request.body.scheduledTimeEnd;
-
-    const data = {
-      name: name,
-      est_time: estTime,
-      due_date: dueDate,
-      description: description,
-      completed: false,
-      priority: priority,
-      scheduled_time_start: scheduledTimeStart,
-      scheduled_time_end: scheduledTimeEnd,
-    };
-
-    // Add a new task
-    const writeResult = admin.firestore().collection('Users').doc(request.decodedClaims.uid).collection('tasks').set(data);
-    console.log("Task has been added,", writeResult.id);
-  } else {
-    response.redirect("/");
-  }
 });
 
 async function addUser(uid, email, displayName){
---
+++ Firebase/public/scripts/tasks.js
@@ -0,0 +1,34 @@
+const { response } = require("express");
+
+function addTask(){
+    document.getElementById("alertbox").hidden = true;
+    document.getElementById("alertbox").classList.remove("alert-danger");
+    document.getElementById("alertbox").classList.remove("alert-success");
+    data = {
+        name:document.getElementById("name").value,
+        description: document.getElementById("description").value,
+        tag: document.getElementById("tag").value,
+        dueDate: document.getElementById("dueDate").value,
+        priority: document.getElementById("priority").value,
+        estTime: document.getElementById("estTime").value,
+        scheduledTimeStart: document.getElementById("scheduledTimeStart").value,
+        scheduledTimeEnd: document.getElementById("scheduledTimeEnd").value
+    };
+
+    fetch('/addTask', {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    }).then(response => response.json())
+    .then(data => {
+        document.getElementById("alertbox").hidden = false;
+        document.getElementById("alertbox").classList.add("alert-success");
+        document.getElementById("alertbox").innerHTML = data;
+    }).catch((error) =>{
+        document.getElementById("alertbox").hidden = false;
+        document.getElementById("alertbox").classList.add("alert-danger");
+        document.getElementById("alertbox").innerHTML = error.message;
+    });
+}
