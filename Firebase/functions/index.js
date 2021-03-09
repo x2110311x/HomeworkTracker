371c35e91236c9ce7e5081d9539fea807a94465e
Alex Sweeney <x2110311x@gmail.com>
2021-03-08 23:01:35 -0500

    Add addUserInfo function and fix addUser

--- Firebase/functions/index.js
+++ Firebase/functions/index.js
@@ -238,24 +238,48 @@ app.get('*', (request, response) => {
 });
 
 exports.addUser = functions.auth.user().onCreate((user) => {
-    admin.auth().getUser(user.uid).then((user) => {
-        if (user.name == null){
-            var uname = user.displayName
-        } else{
-            var uname = user.name
-        }
-        const data = {
+    var uname = user.providerData.displayName;
+    if (uname == null) {
+        var data = {
+            uid: user.uid,
+            email: user.email,
+            creationDate: admin.firestore.Timestamp.now()
+        };
+    } else {
+        var data = {
             uid: user.uid,
             email: user.email,
             name: uname,
             creationDate: admin.firestore.Timestamp.now()
         };
-        const writeResult = admin.firestore().collection('Users').doc(uid).set(data).then(() => {
-            console.log("User added to DB");
-        });
-    }).catch((error) => {
-        console.log("Error recording user. Check name entry in DB");
-        console.log(error);
+    }
+    return admin.firestore().collection('Users').doc(user.uid).set(data).then(() => {
+        console.log('User Add succeeded!');
+      });
+});
+
+exports.addUserInfo = functions.firestore.document("Users/{uid}").onCreate((snap, context) => {
+    const db = admin.firestore();
+    const userData = snap.data();
+    const userID = userData.uid;
+    const docPath = db.collection("Users").doc(userID);
+    return admin.auth().getUser(userID).then((user) => {
+        var userName = user.providerData.displayName;
+        if (userName == null){
+            userName = user.name;
+        }
+        if (userName == null){
+            userName = user.displayName;
+        }
+        if (userName != null){
+            docPath.update({name:userName}).then(() => {
+                console.log('Name update succeeded!');
+              });
+        } else{
+            console.log("User name is null!!!!")
+        }
+    }).then(() => {
+        console.log("Success");
     });
 });
 
