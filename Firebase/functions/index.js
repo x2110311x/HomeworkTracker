f4d8c21d776b5799dc17f602654b99075802b633
woeffner <58149950+woeffner@users.noreply.github.com>
2021-03-09 00:59:51 -0500

    Added custom Tag Feature

--- Firebase/functions/index.js
+++ Firebase/functions/index.js
@@ -231,7 +231,36 @@ app.get('/tasks', checkCookieMiddleware, (request, response) => {
             response.status(403).send("Unauthorized");
         }
     })();
-    });
+});
+
+app.post("/addCustTag", checkCookieMiddleware, (request, response) => {
+    if (request.signedin) {
+        var full_name = request.body.full_name;
+        var short_name = request.body.short_name;
+        var color = request.body.color;
+        var date_created = admin.firestore.Timestamp.now();
+
+        const data = {
+            full_name: full_name,
+            short_name: short_name,
+            color: color,
+            date_created: date_created,
+        };
+
+        // Add a new tag
+        const writeResult = admin.firestore().collection('Users')
+            .doc(request.decodedClaims.uid).collection('tags').add(data)
+            .then(() => {
+                response.status(200).send("Tag successfully added");
+                console.log("Tag has been added,", writeResult.id);
+            }).catch((error) => {
+                console.error("Error writing document: ", error);
+                response.send(500).send("Error writing document: ", error.message);
+            });
+    } else {
+        response.status(403).send("Unauthorized");
+    }
+});
 
 app.get('*', (request, response) => {
     response.status(404).render('404');
