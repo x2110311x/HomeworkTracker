507eb83efd82c306befe536e9906749b3cf8ee08
Alex Sweeney <x2110311x@gmail.com>
2021-03-16 00:51:43 -0400

    Updated ViewTasks to use real data

--- Firebase/functions/webapp/pages/viewTasks.js
+++ Firebase/functions/webapp/pages/viewTasks.js
@@ -2,21 +2,45 @@ module.exports = function (admin, app) {
     app.get('/viewTasks', (request, response) => {
         if (request.signedin) {
             let user = request.decodedClaims;
-            var task1 = {taskName: "Task1", tag:"MATHs", color:"#014d74", description:"Homework1", date:"05/10/21", priority: 3, estTime:900};
-            var task2 = {taskName: "Task2", tag:"CS2", color:"#3389b5", description:"Homework2 is testing testing testing testing testingtesting testing testing testing testingtestingtestingtestingtesting testing testing", date:"05/12/21", priority: 2, estTime:60};
-            
-            var firstSectionTasks = [task1, task2];
 
-            var task3 = {taskName: "Task3", tag:"HIST", color:"#014d74", description:"Homework3", date:"05/10/21", priority: 7, estTime:5};
-            var secondSectionTasks = [task3];
+            var curDay = new Date();
+            var day = String(curDay.getDate()).padStart(2, '0');
+            var month = String(curDay.getMonth() + 1).padStart(2, '0');
+            var year = curDay.getFullYear();
 
-            var section1 = {title: "Due Today", completionPercent: 20, task: firstSectionTasks};
-            var section2 = {title: "Later This Week", completionPercent: 10, task: secondSectionTasks};
-            
-    
-            var sections = [section1, section2];
-
-            response.render('viewTasks', {section:sections});
+            var today = year + '-' + month + '-' + day;
+            var upcomingDays = [];
+            for(i = 0; i < 7; i++){
+                let newDay = year + '-' + month + '-' + String(curDay.getDate() + i).padStart(2, '0');
+                upcomingDays.push(newDay);
+            }
+            let todayTasks = [];
+            let laterTasks = [];
+            admin.firestore().collection('Users').doc(request.decodedClaims.uid).collection('tasks').get()
+            .then(snapshot => {
+                snapshot.forEach((item) => {
+                    let data = item.data();
+                    data.color = "#014d74";
+                    data.tag = "Tag Placeholder";
+                    if (data.due_date == today){
+                        todayTasks.push(data);
+                    } else if (upcomingDays.includes(data.due_date)) {
+                        laterTasks.push(data);
+                    }
+                });
+                var sections = [];
+                if(todayTasks.length > 0){
+                    let section = {title: "Due Today", completionPercent: 20, task: todayTasks};
+                    sections.push(section);
+                }
+                if (laterTasks.length > 0 ){
+                    let section = {title: "Upcoming Tasks", completionPercent: 20, task: laterTasks};
+                    sections.push(section);
+                }
+                response.render('viewTasks', {section:sections});
+            }).catch((error) => {
+                response.status(500).send("Internal Server Error");
+            });
         } else {
             response.redirect('/');
         }
