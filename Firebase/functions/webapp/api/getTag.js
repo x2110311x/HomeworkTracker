4e9f7efce8ec49264e1c3d0fd8914590957f0db2
Alex Sweeney <x2110311x@gmail.com>
2021-03-18 17:49:58 -0400

    Taking a stab at getTag.js

--- Firebase/functions/webapp/api/getTag.js
+++ Firebase/functions/webapp/api/getTag.js
@@ -1,21 +1,27 @@
+const fetch = require('node-fetch');
+
 module.exports = function (admin, router) {
     router.get('/getTag', (request, response) => {
-        (async () => {
-            if (request.signedin) {
-                try {
-                    var tagid = request.query.tagid;
-                    return admin.firestore().collection('Users').doc(request.decodedClaims.uid).collection('tags').doc(tagid).get()
-                    .then(snapshot => {
-                        return response.send(snapshot.data());
-                    });
-                } catch (error) {
-                    console.error("Error retrieving tags: ", error);
-                    response.send(500).send("Error retrieving tags: ", error.message);
+        if (request.signedin) {
+            var tagid = request.query.tagid;
+            fetch(`https://${request.hostname}/api/getTagRef?full_name=${full_name}`,{
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'cookie': req.cookies.__session
                 }
-            }
-            else{
-                response.status(403).send("Unauthorized");
-            }
-        })();
+            }).then((tagid) => {
+                admin.firestore().collection('Users').doc(request.decodedClaims.uid).collection('tags').doc(tagid).get()
+                .then(snapshot => {
+                    return response.send(snapshot.data());
+                })
+            }).catch((error) => {
+                console.error("Error retrieving tags: ", error);
+                response.send(500).send("Error retrieving tags: ", error.message);
+            });
+        }
+        else{
+            response.status(403).send("Unauthorized");
+        }
     });
 }
