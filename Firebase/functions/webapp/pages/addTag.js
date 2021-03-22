aa3015885439d53434ea3cf4f2b8fba8f7bd6e90
Alex Sweeney <x2110311x@gmail.com>
2021-03-21 22:53:59 -0400

    Serve categories to front-end rendering

--- Firebase/functions/webapp/pages/addTag.js
+++ Firebase/functions/webapp/pages/addTag.js
@@ -1,8 +1,24 @@
+const fetch = require('node-fetch');
+
 module.exports = function (admin, app) {
     app.get('/addTag', (request, response) => {
         if (request.signedin) {
-            let user = request.decodedClaims;
-            response.render('addTag');
+            //var url = `https://${request.hostname}`;
+            var url = `http://localhost:5000`;
+            fetch(`${url}/api/getCategories`, {
+                method: 'GET',
+                headers: {
+                    'cookie': `__session=${request.cookies.__session}`
+                }
+            }).then((categoriesResp) => {
+                categoriesResp.text().then((rawjson) => {
+                    var categories = JSON.parse(rawjson);
+                    response.render('addTag', {category: categories});
+                })
+            }).catch((error) => {
+                console.error(`Error retrieving categories: ${error}`);
+                response.status(500).send(`Error retrieving categories: ${error}`);
+            });
         } else {
             response.redirect('/');
         }
