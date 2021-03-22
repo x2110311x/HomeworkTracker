b5e5a192f0c75f65954699945d3a108bd0969212
Alex Sweeney <x2110311x@gmail.com>
2021-03-22 01:06:14 -0400

    Retrieve Tag data and edit link

--- Firebase/functions/webapp/pages/viewTasks.js
+++ Firebase/functions/webapp/pages/viewTasks.js
@@ -1,46 +1,101 @@
+const fetch = require('node-fetch');
+async function getEditLink(admin, name, uid){
+    var retvar = "#";
+    admin.firestore().collection('Users').doc(uid).collection('tasks')
+    .where('name', '==', name).get()
+        .then((snapshot) => {
+        if (snapshot.empty) {
+           retvar = "#";
+        } else {
+            var path = snapshot.docs[0].ref.path.split("/");
+            retvar = `/editTask?id=${path[3]}`;
+        }
+    });
+    return new Promise(resolve => {
+        setTimeout(() => {
+            console.log(retvar);
+            resolve(retvar);
+        }, 500);
+      });
+}
+
+async function getTaskSections(admin, uid){
+    var curDay = new Date();
+    var day = String(curDay.getDate()).padStart(2, '0');
+    var month = String(curDay.getMonth() + 1).padStart(2, '0');
+    var year = curDay.getFullYear();
+
+    var today = year + '-' + month + '-' + day;
+    var upcomingDays = [];
+    for(i = 0; i < 7; i++){
+        let newDay = year + '-' + month + '-' + String(curDay.getDate() + i).padStart(2, '0');
+        upcomingDays.push(newDay);
+    }
+    let todayTasks = [];
+    let laterTasks = [];
+    admin.firestore().collection('Users').doc(uid).collection('tasks').get()
+    .then(snapshot => {
+        snapshot.forEach((item) => {
+            const data = item.data();
+            var task = {
+                description: data.description,
+                due_date: data.due_date,
+                est_time: data.est_time,
+                name: data.name,
+                priority: data.priority,
+                completed: data.completed
+            }
+            getEditLink(admin, task.name, uid).then((editLink) =>{
+                task.editLink = editLink;
+                if(data.tag === undefined){
+                    task.color = "#014d74";
+                    task.tagName = "No Tag";
+                } else {
+                    admin.firestore().doc(data.tag).get().then(snapshot2 => {
+                        let tagData = snapshot2.data();
+                        task.color = tagData.color;
+                        task.tagName = tagData.full_name;
+                    }).then(() => {
+                        console.log(task);
+                        if (data.due_date == today){
+                            todayTasks.push(task);
+                        } else if (upcomingDays.includes(task.due_date)) {
+                            laterTasks.push(task);
+                        }
+                    }).catch((error) => {
+                        console.log("Can't find tag");
+                        task.color = "#014d74";
+                        task.tagName = "No Tag";
+                    });
+                }
+            })
+        });
+    });
+    return new Promise(resolve => {
+        setTimeout(() => {
+          resolve([todayTasks, laterTasks]);
+        }, 2000);
+      });
+}
+
 module.exports = function (admin, app) {
     app.get('/viewTasks', (request, response) => {
         if (request.signedin) {
             let user = request.decodedClaims;
-
-            var curDay = new Date();
-            var day = String(curDay.getDate()).padStart(2, '0');
-            var month = String(curDay.getMonth() + 1).padStart(2, '0');
-            var year = curDay.getFullYear();
-
-            var today = year + '-' + month + '-' + day;
-            var upcomingDays = [];
-            for(i = 0; i < 7; i++){
-                let newDay = year + '-' + month + '-' + String(curDay.getDate() + i).padStart(2, '0');
-                upcomingDays.push(newDay);
-            }
-            let todayTasks = [];
-            let laterTasks = [];
-            admin.firestore().collection('Users').doc(request.decodedClaims.uid).collection('tasks').get()
-            .then(snapshot => {
-                snapshot.forEach((item) => {
-                    let data = item.data();
-                    data.color = "#014d74";
-                    data.tag = "Tag Placeholder";
-                    if (data.due_date == today){
-                        todayTasks.push(data);
-                    } else if (upcomingDays.includes(data.due_date)) {
-                        laterTasks.push(data);
-                    }
-                });
                 var sections = [];
-                if(todayTasks.length > 0){
-                    let section = {title: "Due Today", completionPercent: 20, task: todayTasks};
-                    sections.push(section);
-                }
-                if (laterTasks.length > 0 ){
-                    let section = {title: "Upcoming Tasks", completionPercent: 20, task: laterTasks};
-                    sections.push(section);
-                }
-                response.render('viewTasks', {section:sections});
-            }).catch((error) => {
-                response.status(500).send("Internal Server Error");
-            });
+                getTaskSections(admin, user.uid, request).then((data) => {
+                    var todayTasks = data[0];
+                    var laterTasks = data[1];
+                    if(todayTasks.length > 0){
+                        let section = {title: "Due Today", completionPercent: 20, task: todayTasks};
+                        sections.push(section);
+                    }
+                    if (laterTasks.length > 0 ){
+                        let section = {title: "Upcoming Tasks", completionPercent: 20, task: laterTasks};
+                        sections.push(section);
+                    }
+                    response.render('viewTasks', {section:sections});
+                })
         } else {
             response.redirect('/');
         }
