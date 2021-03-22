bb6a8cdca288d4de79587872f99fe5c4ae7b1834
Alex Sweeney <x2110311x@gmail.com>
2021-03-22 01:05:08 -0400

    Serve data to editTask and load the task ID

--- Firebase/functions/webapp/pages/editTask.js
+++ Firebase/functions/webapp/pages/editTask.js
@@ -2,7 +2,26 @@ module.exports = function (admin, app) {
     app.get('/editTask', (request, response) => {
         if (request.signedin) {
             let user = request.decodedClaims;
-            response.render('editTask');
+            var taskID = request.query.id;
+            console.log(taskID);
+            admin.firestore().collection('Users').doc(user.uid).collection('tasks').doc(taskID).get()
+            .then((snap) => {
+                var data = snap.data();
+                var task = {
+                    id: taskID,
+                    scheduled_time_start: data.scheduled_time_start.replace("-","/").replace("-","/"),
+                    scheduled_time_end: data.scheduled_time_end.replace("-","/").replace("-","/"),
+                    description: data.description,
+                    estTime: data.est_time,
+                    dueDate: data.due_date,
+                    priority: data.priority
+                }
+                admin.firestore().doc(data.tag).get().then(snapshot2 => {
+                    let tagData = snapshot2.data();
+                    task.tag = tagData.full_name;
+                    response.render('editTask', {task, task});
+                });
+            })
         } else {
             response.redirect('/');
         }
