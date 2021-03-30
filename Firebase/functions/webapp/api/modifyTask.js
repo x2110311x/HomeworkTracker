669985199f17531616940c48b82bc997be3ab5dc
Alex Sweeney <x2110311x@gmail.com>
2021-03-30 19:03:24 -0400

    Finish front-end pieces to edit tasks

--- Firebase/functions/webapp/api/modifyTask.js
+++ Firebase/functions/webapp/api/modifyTask.js
@@ -1,7 +1,9 @@
 module.exports = function (admin, router) {
     router.post('/modifyTask', (request, response) => {
         if (request.signedin) {
-            var taskname = request.body.task_name;
+            var user = request.decodedClaims;
+            var taskID = request.body.taskID;
+            var tag = request.body.tag;
             var estTime = request.body.estTime;
             var dueDate = request.body.dueDate;
             var description = request.body.description;
@@ -10,24 +12,40 @@ module.exports = function (admin, router) {
             var scheduledTimeStart = request.body.scheduledTimeStart;
             var scheduledTimeEnd = request.body.scheduledTimeEnd;
             
-            admin.firestore().collection('Users')
-                .doc(request.decodedClaims.uid).collection('tasks')
-                .where('name', '==', taskname).limit(1).get().then((snap) => {
-                    snap.docs[0].ref.update({
-                        est_time: estTime,
-                        due_date: dueDate,
-                        description: description,
-                        completed: completed,
-                        priority: priority,
-                        scheduled_time_start: scheduledTimeStart,
-                        scheduled_time_end: scheduledTimeEnd,
-                    }).then(() => {
+            var docPath = `/Users/${user.uid}/tasks/${taskID}`;
+            console.log(docPath);
+            var data = {};
+            if (estTime != undefined){
+                data['est_time'] = estTime;
+            }
+            if (dueDate != undefined){
+                data['due_date'] = dueDate;
+            }
+            if (description != undefined){
+                data['description'] = description;
+            }
+            if (completed != undefined){
+                data['completed'] = completed;
+            }
+            if (priority != undefined){
+                data['priority'] = priority;
+            }
+            if (scheduledTimeStart != undefined){
+                data['scheduled_time_start'] = scheduledTimeStart;
+            }
+            if (scheduledTimeEnd != undefined){
+                data['scheduled_time_end'] = scheduledTimeEnd;
+            }
+            if (tag != undefined){
+                data['tag'] = tag;
+            }
+            console.log(data);
+            admin.firestore().doc(docPath).update(data).then(() => {
                         response.status(200).send("Task modified successfully");
-                    });
-                }).catch(error => {
+            }).catch(error => {
                     console.error(`Error modifying task: ${error}`);
                     response.status(500).send(`Error modifying task: ${error}`);
-                });
+            });
         } else {
             response.status(403).send("Unauthorized");
         }
