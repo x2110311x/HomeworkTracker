6714945cedfa2eaed7637e2b7fcb8ded34f97b70
Alex Sweeney <x2110311x@gmail.com>
2021-04-27 10:47:33 -0400

    Fix and finish tagEdit

--- Firebase/functions/webapp/api/editTag.js
+++ Firebase/functions/webapp/api/editTag.js
@@ -1,28 +1,35 @@
 module.exports = function (admin, router) {
     router.post("/editTag", (request, response) => {
         if (request.signedin) {
+            var user = request.decodedClaims;
             var full_name = request.body.full_name;
             var short_name = request.body.short_name;
             var color = request.body.color;
-            var date_created = admin.firestore.Timestamp.now();
-            var tagId = request.tagId;
+            var date_updated = admin.firestore.Timestamp.now();
+            var tagId = request.body.tagId;
+            console.log(tagId);
+            console.log(request.body);
 
-            const data = {
-                full_name: full_name,
-                short_name: short_name,
-                color: color,
-                date_created: date_created,
+            var data = {
+                date_updated: date_updated,
             };
+            if (full_name != undefined){
+                data['full_name'] = full_name;
+            }
+            if (short_name != undefined){
+                data['short_name'] = short_name;
+            }
+            if (color != undefined){
+                data['color'] = color;
+            }
 
-            // Update a tag
-            const writeResult = admin.firestore().collection('Users')
-                .doc(request.decodedClaims.uid).collection('tags').where(tagId).set(data)
-                .then(() => {
+            var docPath = `/Users/${user.uid}/tags/${tagId}`;
+            const writeResult = admin.firestore().doc(docPath).update(data).then(() => {
                     response.status(200).send("Tag successfully updated");
                     console.log("Tag has been added,", writeResult.id);
                 }).catch((error) => {
-                    console.error("Error writing document: ", error);
-                    response.status(500).send("Error writing document: ", error.message);
+                    console.error(`Error modifying tag: ${error}`);
+                    response.status(500).send(`Error modifying tag: ${error}`);
                 });
         } else {
             response.status(403).send("Unauthorized");
--- Firebase/functions/webapp/pages/editTag.js
+++ Firebase/functions/webapp/pages/editTag.js
@@ -1,3 +1,5 @@
+const fetch = require('node-fetch');
+
 module.exports = 
 /**
  * @description Retrieves data and renders page to edit a tag. If not signed in, redirects to the homepage.
@@ -7,21 +9,39 @@ module.exports =
 function editTag(admin, app) {
     app.get('/editTag', (request, response) => {
         if (request.signedin) {
-          //  let user = request.decodedClaims;
-          //  var taskID = request.query.id;
-         //   admin.firestore().collection('Users').doc(user.uid).collection('tasks').doc(taskID).get()
-        //    .then((snap) => {
-          //      var data = snap.data();
-                var tag = {
-                    full_name: "FullName",
-                    short_name: "ShortName",
-                    color: "#565555"
-                }
-        
+            let user = request.decodedClaims;
+            var tagID = request.query.id;
+            if (tagID != undefined){
+                admin.firestore().collection('Users').doc(user.uid).collection('tags').doc(tagID).get()
+                .then((snap) => {
+                    var data = snap.data();
+                    var tag = {
+                        full_name: data['full_name'],
+                        short_name: data['short_name'],
+                        color: data['color'],
+                        id: tagID
+                    };
                     response.status(200).render('editTag', {tag: tag});
+                });
+            } else{
+                var url = "https://homeworktracker-b9805.web.app";
+                fetch(`${url}/api/getTags`, {
+                    method: 'GET',
+                    headers: {
+                        'cookie': `__session=${request.cookies.__session}`
+                    }
+                }).then((tagResp) => {
+                    tagResp.text().then((rawjson) => {
+                        var tags = JSON.parse(rawjson);
+                        response.status(200).render('editTag', {tags: tags});
+                    })
+                }).catch((error) => {
+                    console.error(`Error retrieving categories: ${error}`);
+                    response.status(500).send(`Error retrieving categories: ${error}`);
+                });
             }
-            else {
-                response.redirect('/');
-            }
-        });
-        } 
+        } else {
+            response.redirect('/');
+        }
+    });
+}
