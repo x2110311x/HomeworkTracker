520f23eeb122ca1f69dcdd26c11e11c1899446c5
Alex Sweeney <x2110311x@gmail.com>
2021-04-27 18:10:55 -0400

    Update Freetime

--- Firebase/functions/webapp/pages/freetime.js
+++ Firebase/functions/webapp/pages/freetime.js
@@ -8,32 +8,17 @@ module.exports =
 function freetime(admin, app) {
     app.get('/freetime', (request, response) => {
         if (request.signedin) {
-            try {
             let user = request.decodedClaims;
             var week = request.query.week;
-            /*
-            var searchWeek = request.week_info.text;
-            searchWeek = searchWeek.split('{')[1];
-            searchWeek = searchWeek.split('-')[0];
-            searchWeek = searchWeek + ", 2020";
-            var searchDate = new Date(searchWeek);
-            var searchString = (searchDate.getMonth() < 10 ? '0' + searchDate.getMonth() : searchDate.getMonth());
-            searchString += (searchDate.getDay() < 10 ? '0' + searchDate.getDay() : searchDate.getDay());
-            // need a way to retrieve year
-            searchString += "2021";
-            */
-            var dateTmp = "04-05-2021"
 
-            admin.firestore().collection('Users').doc(user.uid).collection('freetime').doc(dateTmp).get()
+            admin.firestore().collection('Users').doc(user.uid).collection('freetime').doc(week).get()
                 .then((snapshot) => {
                     var day = snapshot.data();
-                    console.log(day);
-                    response.status(200).render('freetime', { days: day });
-                })
-            } catch (error) {
+                    response.status(200).render('freetime', { week: week, days: day });
+                }).catch((error) => {
                 console.error(`Error retrieving freetime`);
                 response.status(500).send(error);
-            }
+            });
         } else {
             response.redirect('/');
         }
